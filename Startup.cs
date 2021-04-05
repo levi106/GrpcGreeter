ab5d96d4036988a1adcefa616285d3d4a72931ef
levi106 <crystalline407@gmail.com>
2021-04-05 22:03:07 +0900

    enable OpenTelemetry

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Trace;
 
 namespace GrpcGreeter
 {
@@ -16,6 +17,11 @@ namespace GrpcGreeter
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOpenTelemetryTracing(
+                (builder) => builder
+                    .AddAspNetCoreInstrumentation()
+                    .AddConsoleExporter()
+            );
             services.AddGrpc();
             services.AddGrpcReflection();
         }
