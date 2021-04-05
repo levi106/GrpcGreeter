fe3cada42267ecfd0967ae40c83435741e2a59fd
levi106 <crystalline407@gmail.com>
2021-04-06 01:44:58 +0900

    add more methods

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
@@ -10,6 +11,8 @@ namespace GrpcGreeter
     public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
+        private int _count;
+
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
@@ -21,6 +24,33 @@ namespace GrpcGreeter
             {
                 Message = "Hello " + request.Name
             });
+        }
+
+        public override Task<CounterReply> IncrementCount(Empty request, ServerCallContext context)
+        {
+            _logger.LogInformation("Incrementing count by 1");
+            _count += 1;
+            return Task.FromResult(new CounterReply { Count = _count });
+        }
+
+        public override async Task<CounterReply> AccumulateCount(IAsyncStreamReader<CounterRequest> requestStream, ServerCallContext context)
+        {
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                _logger.LogInformation($"Incrementing count by {message.Count}");
+                _count += message.Count;
+            }
+
+            return new CounterReply { Count = _count };
+        }
+
+        public override async Task Countdown(Empty request, IServerStreamWriter<CounterReply> responseStream, ServerCallContext context)
+        {
+            for (var i = _count; i >= 0; i--)
+            {
+                await responseStream.WriteAsync(new CounterReply { Count = i });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
         }
     }
 }
